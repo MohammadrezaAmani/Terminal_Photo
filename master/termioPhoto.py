8e38881410e32108e4e8f476667a75215155fb17
MohammadrezaAmani <more.amani@yahoo.com>
2022-02-10 11:09:43 +0330

    logic

---
+++ master/termioPhoto.py
@@ -0,0 +1,7 @@
+#logic: 
+#   1- read photo
+#   2- resize photo
+#   3- convert photo to numpy array
+#   4- for each pixel in photo use set_color to set color of word in text
+#   5- save it as .sh file
+#   6- run it
