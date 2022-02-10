f41231118e41ac18f85277d7e491d5e4506c92eb
MohammadrezaAmani <more.amani@yahoo.com>
2022-02-10 11:46:01 +0330

    setters and getters- update

--- master/termioPhoto.py
+++ master/termioPhoto.py
@@ -15,59 +15,77 @@ from numpy import asarray
 
 #--------------------------create class and functions---------------------------
 
+
 class Photo:
-    def __init__(self,path = 'IMG.jpg', text=" ", size='30*50', front=False,
-                 height = 50, width = 30):
+    def __init__(self, path='IMG.jpg', text=" ", size='30*50', front=False):
         self.__path = path
         self.__text = text
         self.__size = size
         self.__front = front
-        self.__height = height
-        self.__width = width
-        
-    #setters
-    
+        self.__height = size.split('*')[1]
+        self.__width = size.split('*')[0]
+
+    #------------setters------------
+    # 1. path setter
     def set_path(self, path):
         if type(path) == str:
             self.__path = path
         else:
             print("path must be a string")
+    # 2. text setter
 
     def set_text(self, text):
         self.__text = str(text)
+    # 3. size setter
 
     def set_size(self, size):
-        self.__size = size
+        if type(size) == str:
+            if size.find('*') != -1:
+                self.__size = size
+            else:
+                print("size must be a string in format 'width*height' exapmle: '30*50'")
+        else:
+            print("size must be a string")
+    # 4. front setter (True or False)
 
     def set_front(self, front):
         if type(front) == bool:
             self.__front = front
         else:
             print("front must be a boolean")
+    # 5.height settter
 
     def set_height(self, height):
         if type(height) == float or type(height) == int:
             self.__height = height
+    # 6. width setter
 
     def set_width(self, width):
         if type(width) == float or type(width) == int:
             self.__width = width
-    #getters
+    #-----------------getters-----------------
+    # 1. path getter
 
     def get_path(self):
         return self.__path
+    # 2. text getter
 
     def get_text(self):
         return self.__text
+    # 3. size getter
 
     def get_size(self):
         return self.__size
+    # 4. front getter
 
     def get_front(self):
         return self.__front
+    # 5. height getter
 
     def get_height(self):
         return self.__height
+    # 6. width getter
 
     def get_width(self):
         return self.__width
+    
