08db7ea23102d8be0196d639bace35b238d59ec0
MohammadrezaAmani <more.amani@yahoo.com>
2022-02-10 11:33:10 +0330

    setters and getters

--- master/termioPhoto.py
+++ master/termioPhoto.py
@@ -24,3 +24,50 @@ class Photo:
         self.__front = front
         self.__height = height
         self.__width = width
+        
+    #setters
+    
+    def set_path(self, path):
+        if type(path) == str:
+            self.__path = path
+        else:
+            print("path must be a string")
+
+    def set_text(self, text):
+        self.__text = str(text)
+
+    def set_size(self, size):
+        self.__size = size
+
+    def set_front(self, front):
+        if type(front) == bool:
+            self.__front = front
+        else:
+            print("front must be a boolean")
+
+    def set_height(self, height):
+        if type(height) == float or type(height) == int:
+            self.__height = height
+
+    def set_width(self, width):
+        if type(width) == float or type(width) == int:
+            self.__width = width
+    #getters
+
+    def get_path(self):
+        return self.__path
+
+    def get_text(self):
+        return self.__text
+
+    def get_size(self):
+        return self.__size
+
+    def get_front(self):
+        return self.__front
+
+    def get_height(self):
+        return self.__height
+
+    def get_width(self):
+        return self.__width
